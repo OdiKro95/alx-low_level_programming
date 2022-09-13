9494a76eb22401b1431a4203bd3dc75e4e22b624
OdiKro95 <skyere80@gmail.com>
2022-09-13 15:52:12 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - this code prints the word _putchar
+ *
+ * Return: Always 0, when sucessful.
+*/
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
